81d2bafff3878d1168bd1c45972dbf0b2c31cb4a
IllusiveAldebaran <franciscofabiangutierrez@gmail.com>
2025-12-04 04:24:37 -0800

    Trying to get Transpose

---
+++ test_score_layer.py
@@ -0,0 +1,143 @@
+
+# from ml_dtypes import bfloat16
+import numpy as np
+import sys
+import os
+from utils.tiling import tile_matrix
+
+import aie.iron as iron
+from aie.iron import ExternalFunction, jit
+from aie.iron import Kernel, ObjectFifo, Program, Runtime, Worker
+from aie.iron.placers import SequentialPlacer
+from aie.iron.controlflow import range_
+from aie.helpers.taplib import TensorAccessPattern, TensorTiler2D
+from aie.utils.config import cxx_header_path
+
+
+# JIT decorator for IRON
+# Decorator to compile an IRON kernel into a binary to run on the NPU.
+# Parameters:
+#     - is_placed (bool): Whether the kernel is using explicit or deferred placement API. Defaults to True.
+#     - use_cache (bool): Use cached MLIR module if available. Defaults to True.
+@iron.jit(is_placed=False)
+def scores(input0, output):
+    N = input0.shape[0]  # Tensor size
+    N_out = output.shape[0]
+    element_type = output.dtype
+
+    tap_in = TensorTiler2D.simple_tiler(input0.shape, tile_col_major=True)[0];
+
+    # --------------------------------------------------------------------------
+    # In-Array Data Movement
+    # --------------------------------------------------------------------------
+
+    in_ty = np.ndarray[(N,), np.dtype[element_type]]
+    out_ty = np.ndarray[(N_out,), np.dtype[element_type]]
+
+    #of_x = ObjectFifo(in_ty, name="x")
+    #of_z = ObjectFifo(out_ty, name="z")
+
+    of_x = ObjectFifo(in_ty)
+    of_z = of_x.cons().forward()
+
+    # --------------------------------------------------------------------------
+    # Task each core will run
+    # --------------------------------------------------------------------------
+
+    # The kernel acquires input tensors X and Y, and output tensor Z, performs the
+    # SAXPY operation on X and Y, and writes the result in Z.
+
+    dense_ly_kernel = ExternalFunction(
+        "scores",
+        source_file=os.path.join(os.path.dirname(__file__), "iron_kernels/scores_kernel.cc"),
+        arg_types=[in_ty, out_ty],
+        include_dirs=[
+            cxx_header_path(),
+            os.path.join(os.path.dirname(__file__), "iron_kernels")
+        ],
+    )
+
+    def core_body(of_x, of_z, dense_ly_kernel):
+        elem_x = of_x.acquire(1)
+        elem_z = of_z.acquire(1)
+        dense_ly_kernel(elem_x, elem_z)
+        of_x.release(1)
+        of_z.release(1)
+
+    worker = Worker(
+        core_body, fn_args=[of_x.cons(), of_z.prod(), dense_ly_kernel]
+    )
+
+    # --------------------------------------------------------------------------
+    # DRAM-NPU data movement and work dispatch
+    # --------------------------------------------------------------------------
+
+    rt = Runtime()
+    with rt.sequence(in_ty, in_ty, out_ty) as (a_x, _, c_z):
+        rt.start(worker)
+        rt.fill(of_x.prod(), a_x, tap_in)
+        rt.drain(of_z.cons(), c_z, wait=True)
+
+    # --------------------------------------------------------------------------
+    # Place and generate MLIR program
+    # --------------------------------------------------------------------------
+
+    my_program = Program(iron.get_current_device(), rt)
+    return my_program.resolve_program(SequentialPlacer())
+
+
+def main():
+    element_type = np.int8
+    
+    ###inp = np.loadtxt("./iron_kernels/test_data/dense_input.txt", dtype=np.int8)
+    ###ref = np.loadtxt("./iron_kernels/test_data/dense_out_ref.txt", dtype=np.int8).flatten()
+
+    ###INPUT_ROWS = 160
+    ###INPUT_COLS = 8
+    ###OUTPUT_SIZE = 160 * 64
+
+    ###if inp.size != INPUT_ROWS * INPUT_COLS:
+    ###    raise ValueError(f"input size {inp.size} != {INPUT_ROWS*INPUT_COLS}")
+
+    ###inp_mat = inp.reshape(INPUT_ROWS, INPUT_COLS)
+    ###inp_tiled = tile_matrix(inp_mat, 4, 8)  # flattened tiled input
+
+    Q_np = np.array([[1,  2,   3,  4],
+                     [5,  6,   7,  8],
+                     [9,  10, 11, 12],
+                     [13, 14, 15, 16],
+                     [17, 18, 19, 20],
+                     [21, 22, 23, 24],
+                     [25, 26, 27, 28],
+                     [29, 30, 31, 32]])
+
+    # Convert/set Iron tensors for kernel input and output
+    Q_iron = iron.tensor(Q_np, dtype=np.int8, device="npu")
+    S_iron = iron.zeros((Q_np.shape[1], Q_np.shape[0]), dtype=element_type, device="npu")
+
+    print(Q_iron.shape);
+    print(S_iron.shape)
+    # Insantiate AIE Kernel
+    scores(Q_iron, S_iron)
+
+    print(Q_np);
+    print(S_iron);
+    #out_np = np.array(output, dtype=np.int8)
+
+    errors = 0
+    #for i, (a, r) in enumerate(zip(out_np, ref)):
+    #    if a != r:
+    #        print(f"Error at {i}: {a} != {r}")
+    #        errors += 1
+
+    if errors == 0:
+        print("\nPASS!\n")
+        sys.exit(0)
+    else:
+        print(f"\nError count: {errors}")
+        print("failed.\n")
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
